08e59644848d40f2fce86b01564d81738a999cec
slicky1 <okunonke@gmail.com>
2021-03-17 21:57:31 -0400

    Assigments Completed

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
-// Write your solution here!
+
+   let cats =
+    ["Milo", "Otis", "Garfield"]
+
+    beforeEach(function () {
+        cats.length = 0;
+  
+        cats.push('Milo', 'Otis', 'Garfield');
+      });
+   
+   
+   function destructivelyAppendCat(name) {
+       cats.push(name)
+   }
+        destructivelyAppendCat('Ralph');
+
+        destructivelyAppendCat('Ralph');
+
+function destructivelyPrependCat(name){
+
+    cats.unshift(name);
+
+}
+
+    destructivelyPrependCat("Bob");
+
+    
+
+function destructivelyRemoveLastCat() {
+
+    cats.pop()
+
+}
+
+    destructivelyRemoveLastCat();
+
+    
+
+function destructivelyRemoveFirstCat() {
+
+    cats.shift()
+
+}
+
+   
+
+function appendCat(name) {
+
+  return [...cats, name];
+
+}
+
+    appendCat("Broom");
+
+function prependCat(name) {
+
+    return [name, ...cats];
+
+}
+
+    prependCat("Arnold");
+
+function removeLastCat() {
+
+    return cats.slice(0,2)
+
+}
+
+    removeLastCat();
+
+function removeFirstCat() {
+
+    return cats.slice(1);
+
+}
+
+removeFirstCat();
+
+   
